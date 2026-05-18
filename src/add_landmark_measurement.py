e0370de3ee3aeeea5b483c39655189c373c6c693
Gábor Bezsilla <g.bezsilla@student.tue.nl>
2026-05-18 18:57:53 +0200

    Fixed issue in add_pose 4 and finished add landmark (2a,2b)

--- src/add_landmark_measurement.py
+++ src/add_landmark_measurement.py
@@ -9,7 +9,13 @@ MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (
 
 def add_landmark_measurement(graph, initial_estimate, result):
     # Determine the correct rotation (bearing) and distance from X(4) to L(2) 
-    # rotation = 
-    # distance = 
-    graph.add(gtsam.BearingRangeFactor2D(X(4), L(2), gtsam.Rot2.fromDegrees(rotation), distance, MEASUREMENT_NOISE))
+    # Get actual objects from Values
+    pose = result.atPose2(X(4))
+    landmark = result.atPoint2(L(2))
+
+    # Compute bearing and range
+    rotation = pose.bearing(landmark)
+    distance = pose.range(landmark)
+
+    graph.add(gtsam.BearingRangeFactor2D(X(4), L(2), rotation, distance, MEASUREMENT_NOISE))
     return graph
